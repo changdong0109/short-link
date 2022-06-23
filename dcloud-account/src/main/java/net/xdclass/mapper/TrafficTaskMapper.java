c0f440c765c5a66e61cf3e2ed08409348536be07
changdong <changdong@163.com>
2022-06-23 10:01:49 +0800

    改包名

---
+++ dcloud-account/src/main/java/net/xdclass/mapper/TrafficTaskMapper.java
@@ -0,0 +1,17 @@
+package net.xdclass.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import net.xdclass.model.TrafficTaskDO;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author 二当家小D
+ * @since 2021-11-09
+ */
+public interface TrafficTaskMapper extends BaseMapper<TrafficTaskDO> {
+
+}
